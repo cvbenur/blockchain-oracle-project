64fd6b42c2290194977b43f2eddb8b7ad012927f
Ruben Nabet <rubenabet@gmail.com>
2022-02-25 17:15:40 +0100

    feat(smart-contracts): update data model in TS app

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { fetchCharacters } from "./functions/characters.functions";
+import { fetchCharactersData } from "./functions/characters.functions";
 
-fetchCharacters({ name: 'Spider-Man (Peter Parker)' }).then(console.log).catch(console.error);
+fetchCharactersData({ name: 'Daredevil' }).then(console.log).catch(console.error);
 
 export default {};
