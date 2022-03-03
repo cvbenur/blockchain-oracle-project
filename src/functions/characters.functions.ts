d3f3b42a07ada8e304b92c2ecd386b04537e94e5
Ruben Nabet <rubenabet@gmail.com>
2022-03-03 13:57:15 +0100

    feat: add hard-coded 100 character limit to fetch function per API guidelines

--- src/functions/characters.functions.ts
+++ src/functions/characters.functions.ts
@@ -2,7 +2,7 @@ import { requestBuilder, optionsToParamArray, RequestOptions } from "./request.f
 import { MarvelCharacter } from "../models/marvel-character.model";
 import axios from 'axios';
 
-interface CharacterOptions extends RequestOptions {
+interface FetchCharacterOptions extends RequestOptions {
   offset?: number;
   limit?: number;
   name?: string;
@@ -14,10 +14,20 @@ const re = /\"/gi;
 
 /**
  * Fetches character data from the Marvel Comics API
- * @param options {@link CharacterOptions}
+ * @param options {@link FetchCharacterOptions}
  * @returns An array of character data
+ * 
+ * @example
+ * fetchCharactersData({ name: 'Daredevil' }).then((data) => { console.log(data) });
+ * or:
+ * const data = await fetchCharactersData({ limit: 35 });
  */
-export async function fetchCharactersData(options?: CharacterOptions): Promise<MarvelCharacter[]> {
+export async function fetchCharactersData(options?: FetchCharacterOptions): Promise<MarvelCharacter[]> {
+  if (options?.limit) {
+    // Make sure that we don't request more than 100 characters at once
+    options.limit = options.limit > 100 ? 100 : options.limit;
+  }
+  
   // Craft HTTP request for the characters endpoint
   const req = requestBuilder(
     '/characters',
