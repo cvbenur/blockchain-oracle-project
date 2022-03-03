1812e9fb93688b3b95fdfd9dc42b64921dcbc36f
Ruben Nabet <rubenabet@gmail.com>
2022-03-03 21:37:03 +0100

    tests: add test for getAddress()

--- test/contracts/marvel-character-oracle.test.js
+++ test/contracts/marvel-character-oracle.test.js
@@ -5,10 +5,21 @@ contract('MarvelCharactersOracle', (accounts) => {
     contract = await ORACLE.deployed();
   });
 
+  // Testing testConnection()
   it('should connect to the smart contract', async () => {
     const connection = await contract.testConnection();
 
     assert.equal(connection, true);
+  });
+
+
+  // Testing getAddress()
+  it('should return the correct contract address', async () => {
+    const expected = await contract.address;
+
+    const actual = await contract.getAddress();
+
+    assert.equal(actual, expected);
   });
 
   // Write tests here
